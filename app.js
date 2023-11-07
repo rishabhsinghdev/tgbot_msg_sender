2263266837e9bd63d5a19df75ac08ec32eb688a5
RishabhDev <128444911+rishabhsinghdev@users.noreply.github.com>
2023-11-07 10:31:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,52 +1,49 @@
-module.exports = (req, res) => {
-    const express = require('express');
-    const bodyParser = require('body-parser');
-    const axios = require('axios');
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const path = require('path'); // Import the 'path' module
 
-    const app = express();
-    app.use(bodyParser.urlencoded({ extended: false }));
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
-    const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
-    const CHAT_ID = process.env.CHAT_ID;
-if (req.url === '/') {
-    // Serve the index.html file
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-} else {
-    // Handle other routes or display a 404 message
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
-}  else if (req.url === '/send' && req.method === 'POST') {
-        let body = '';
-        req.on('data', (data) => {
-            body += data;
-        });
-        req.on('end', () => {
-            const message = decodeURIComponent(body);
-            // Send the message to your Telegram bot using axios
-            const telegramApiUrl = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
-            const data = new URLSearchParams({
-                chat_id: CHAT_ID,
-                text: message,
-            }).toString();
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const CHAT_ID = process.env.CHAT_ID;
 
-            axios
-                .post(telegramApiUrl, data, {
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                    },
-                })
-                .then((response) => {
-                    console.log(response.data);
-                    res.end('Message sent to Telegram bot!');
-                })
-                .catch((error) => {
-                    console.error(error);
-                    res.writeHead(500, { 'Content-Type': 'text/plain' });
-                    res.end('Error sending message to Telegram bot');
-                });
-        });
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Not Found');
-    }
-};
+app.use(express.static(path.join(__dirname, 'public')); // Serve static files from the 'public' directory
+
+app.post('/send', (req, res) => {
+    let body = '';
+    req.on('data', (data) => {
+        body += data;
+    });
+    req.on('end', () => {
+        const message = decodeURIComponent(body);
+        // Send the message to your Telegram bot using axios
+        const telegramApiUrl = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
+        const data = new URLSearchParams({
+            chat_id: CHAT_ID,
+            text: message,
+        }).toString();
+
+        axios
+            .post(telegramApiUrl, data, {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            })
+            .then((response) => {
+                console.log(response.data);
+                res.end('Message sent to Telegram bot!');
+            })
+            .catch((error) => {
+                console.error(error);
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Error sending message to Telegram bot');
+            });
+    });
+});
+
+const port = process.env.PORT || 3000; // Use the PORT environment variable for Heroku deployment
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
