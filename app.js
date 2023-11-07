278f33bb9cbd7bc9cc440ff41695c8c18844336b
RishabhDev <128444911+rishabhsinghdev@users.noreply.github.com>
2023-11-07 10:02:36 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,48 @@
+module.exports = (req, res) => {
+    const express = require('express');
+    const bodyParser = require('body-parser');
+    const axios = require('axios');
+
+    const app = express();
+    app.use(bodyParser.urlencoded({ extended: false }));
+
+    const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+    const CHAT_ID = process.env.CHAT_ID;
+
+    if (req.url === '/') {
+        res.end('This is your homepage. You can serve your HTML here.');
+    } else if (req.url === '/send' && req.method === 'POST') {
+        let body = '';
+        req.on('data', (data) => {
+            body += data;
+        });
+        req.on('end', () => {
+            const message = decodeURIComponent(body);
+            // Send the message to your Telegram bot using axios
+            const telegramApiUrl = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
+            const data = new URLSearchParams({
+                chat_id: CHAT_ID,
+                text: message,
+            }).toString();
+
+            axios
+                .post(telegramApiUrl, data, {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                    },
+                })
+                .then((response) => {
+                    console.log(response.data);
+                    res.end('Message sent to Telegram bot!');
+                })
+                .catch((error) => {
+                    console.error(error);
+                    res.writeHead(500, { 'Content-Type': 'text/plain' });
+                    res.end('Error sending message to Telegram bot');
+                });
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+};
